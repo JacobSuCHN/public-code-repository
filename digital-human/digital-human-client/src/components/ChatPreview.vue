539e9baae63554f8df4f2cfa21f5038868b57054
JacobSu <suxinkevin51@163.com>
2025-09-07 19:28:00 +0800

    更新 文件夹 digital-human/digital-human-client

--- digital-human/digital-human-client/src/components/ChatPreview.vue
+++ digital-human/digital-human-client/src/components/ChatPreview.vue
@@ -107,19 +107,23 @@ onMounted(() => {
         <div class="text-sm text-gray-500 mt-2">
           <span class="flex items-center">
             <span class="mr-1">🎙️</span>
-            按住说话 ·
-            <kbd class="ml-1 px-1 py-0.5 bg-gray-200 rounded text-xs">Shift+Q</kbd> 语音输入
+            <span class="mx-1">按住说话 ·</span>
+            <kbd class="ml-1 px-1 py-0.5 bg-gray-200 rounded text-xs">Shift+Q</kbd>
+            <span class="mx-1">语音输入</span>
           </span>
         </div>
         <div class="text-sm text-gray-500 mt-2">
           <span class="flex items-center">
             <span class="mr-1">✏️</span>
-            输入文字后按 <kbd class="mx-1 px-1 py-0.5 bg-gray-200 rounded text-xs">Enter</kbd>
+            <span class="mx-1">输入文字后按</span>
+            <kbd class="mx-1 px-1 py-0.5 bg-gray-200 rounded text-xs">Enter</kbd>
             <span class="mx-1">或</span>
-            <div class="inline-flex items-center justify-center p-[0.3vw] rounded-full bg-[#8b5cf6]">
+            <div
+              class="inline-flex items-center justify-center p-[0.3vw] rounded-full bg-[#8b5cf6]"
+            >
               <img :src="SendSVG" class="w-[0.6vw] h-[0.6vw]" alt="" />
             </div>
-            <div class="ml-1">开始对话</div>
+            <div class="mx-1">开始对话</div>
           </span>
         </div>
       </div>
